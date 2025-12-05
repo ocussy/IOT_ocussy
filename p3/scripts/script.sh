98a6331a0d0ac415bdf2a70a59240ee8a2c8ac11
coco <coco@VMcoco.myguest.virtualbox.org>
2025-12-05 14:33:29 +0100

    good repo

--- p3/scripts/script.sh
+++ p3/scripts/script.sh
@@ -1,59 +1,66 @@
 #!/bin/bash
 
-# 1. Update system
-echo "📦 Updating system..."
+# update system
+echo "Updating system..."
 sudo apt-get update
 sudo apt-get install -y curl
 
-# 2. Install Docker (Universal Method)
-# Using get-docker.sh is safer than apt-repo for Debian Trixie/Ubuntu mix
-echo "🐳 Installing Docker..."
+# install docker
+echo "Installing Docker..."
 if ! command -v docker &> /dev/null; then
     curl -fsSL https://get.docker.com -o get-docker.sh
     sudo sh get-docker.sh
     sudo usermod -aG docker $USER
 else
-    echo "✅ Docker is already installed."
+    echo "Docker is already installed."
 fi
 
-# 3. Install K3d and Kubectl
-echo "🔧 Installing K3d and Kubectl..."
+# install K3d and Kubectl
+echo "Installing K3d and Kubectl..."
 curl -s https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 
-# Install Kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 rm kubectl
 
-# 4. Clean up old cluster (FIX "Already Exists" error)
-echo "🧹 Cleaning up old cluster..."
+# destroy cluster if already exists
+echo "Cleaning up old cluster..."
 k3d cluster delete gitops-cluster 2> /dev/null || true
 
-# 5. Create K3d Cluster
-echo "🚀 Creating K3d cluster..."
-# We disable Traefik to avoid conflicts
-k3d cluster create gitops-cluster \
-    --k3s-arg "--disable=traefik@server:0"
+# create a new K3d cluster
+echo "Creating K3d cluster..."
+# # We disable Traefik to avoid conflicts
+k3d cluster create gitops-cluster --k3s-arg "--disable=traefik@server:0"
 
-# 6. Create Namespaces
-echo "📂 Creating namespaces..."
+# create argocd namespace
+echo "Creating namespaces..."
 kubectl create namespace argocd
 
-# 7. Install ArgoCD
-echo "🐙 Installing ArgoCD..."
+# install ArgoCD
+echo "Installing ArgoCD..."
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
-# Wait for ArgoCD to be ready
-echo "⏳ Waiting for ArgoCD server to start (timeout 5m)..."
+# wait for ArgoCD to be ready
+echo "Waiting for ArgoCD server to start (timeout 5m)..."
 kubectl wait --for=condition=available deployment/argocd-server -n argocd --timeout=300s
 
-#8. Deploy Application via ArgoCD
-echo "📥 Deploying application via ArgoCD..."
+# deploy application via ArgoCD
+echo "Deploying application via ArgoCD..."
 kubectl apply -f ../confs/app.yaml
 
-# 8. Get Admin Password (FIXED METHOD)
-# We use kubectl because 'argocd' CLI is not installed
-echo "🔑 Retrieving admin password..."
+# waiting for the app to be synced and running
+echo "Waiting for ArgoCD to sync and App to be ready..."
+
+while ! kubectl get ns dev > /dev/null 2>&1; do
+    sleep 2
+done
+
+kubectl wait --for=condition=available deployment --all -n dev --timeout=300s
+echo "App is synced and running!"
+
+# get Admin Password
+# we use kubectl because 'argocd' CLI is not installed
+echo "Retrieving admin password..."
 PASSWORD=$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)
 
 echo ""
